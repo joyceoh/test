9708fb981966adb65374c501b2804e2fa1da9fa6
joyceoh <joyce.oh@pendo.io>
2025-05-15 14:09:43 -0400

    added variable in github

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-require('dotenv').config()
-function(process.env.PENDO_APP){
+function(PENDO_APP){
   (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
       o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
